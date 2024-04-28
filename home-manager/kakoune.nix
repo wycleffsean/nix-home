4c76ced45df854104b7f617f11fdaf1d3402a36c
Sean Carey <wycleffsean@gmail.com>
2024-04-28 00:37:34 -0400

    Kakoune: bring in dotfiles

--- home-manager/kakoune.nix
+++ home-manager/kakoune.nix
@@ -11,12 +11,45 @@
   programs.kakoune = {
       enable = true;
       defaultEditor = true;
-      plugins = with pkgs; [
+      config = {
+        colorScheme = "snazzy";
+      };
+      plugins = with pkgs.kakounePlugins; [
           kak-lsp
           kak-fzf
           kak-powerline
       ];
       extraConfig = ''
+        # Use ripgrep instead of grep
+        set-option global grepcmd 'rg -Hn --no-heading'
+
+        # in lieu of autoload
+        # https://github.com/andreyorst/plug.kak/issues/83#issuecomment-864600718
+        evaluate-commands %sh{
+            config_files="
+                rspec.kak
+                ide.kak
+                keybinds.kak
+            "
+
+            for file in $config_files; do
+                printf "%s" "
+                    try %{
+                        source %{''${kak_config:?}/$file}
+                    } catch %{
+                        echo -debug %val{error}
+                    }
+                "
+            done
+        }
       '';
+  };
+
+  xdg.configFile = {
+
+    "kak/colors/snazzy.kak".source = ../dotfiles/kak/colors/snazzy.kak;
+    "kak/rspec.kak".source = ../dotfiles/kak/rspec.kak;
+    "kak/ide.kak".source = ../dotfiles/kak/ide.kak;
+    "kak/keybinds.kak".source = ../dotfiles/kak/keybinds.kak;
   };
 }
