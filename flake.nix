7d80fa4164c727dc896f735b4f68ddfd05989e7a
Sean Carey <wycleffsean@gmail.com>
2024-09-14 15:01:33 -0400

    home-manager: config for sean-Arch PC

--- flake.nix
+++ flake.nix
@@ -37,6 +37,13 @@
     # Standalone home-manager configuration entrypoint
     # Available through 'home-manager --flake .#your-username@your-hostname'
     homeConfigurations = {
+      "sean@sean-Arch" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
+        extraSpecialArgs = {inherit inputs outputs;};
+        # > Our main home-manager configuration file <
+        modules = [./shared/home-manager/home.nix];
+      };
+
       "sean@nixos" = home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.x86_64-linux; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {inherit inputs outputs;};
