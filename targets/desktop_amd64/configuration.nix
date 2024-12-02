9c728e3b823c6f70b51ddc75d39dc25683c7306d
Sean Carey <wycleffsean@gmail.com>
2024-12-01 21:16:32 -0500

    desktop: install discord and mullvad

--- targets/desktop_amd64/configuration.nix
+++ targets/desktop_amd64/configuration.nix
@@ -149,7 +149,9 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+   discord
    kakoune # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+   mullvad-vpn
    ventoy-full
   #  wget
   ];
