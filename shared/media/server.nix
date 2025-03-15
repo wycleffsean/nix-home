1abd57620869da781e1c6ba4dab5bb4bc1bec117
Sean Carey <wycleffsean@gmail.com>
2025-03-14 22:02:51 -0400

    media: proxy web services and add diagnostic web ui

--- shared/media/server.nix
+++ shared/media/server.nix
@@ -3,11 +3,44 @@
 {
   imports = [];
 
+  # Firewall settings to allow HTTP access
+  networking.firewall.allowedTCPPorts = [ 80 443 ];
+
+  # Enable Glances with Web UI
+  services.glances = {
+    enable = true;
+    webUI = {
+      enable = true;
+      port = 61208;
+    };
+  };
+
   services.jellyfin = {
       enable = true;
       openFirewall = true;
   };
   services.jellyseerr.enable = true;
+
+  services.nginx = {
+    enable = true;
+    virtualHosts = {
+      "raspberrypi4.local" = {
+        root = "${builtins.toString ../static}";
+        index = "index.html";
+
+        # Proxy Jellyfin
+        locations."/jellyfin/" = {
+          proxyPass = "http://localhost:8096/";
+          proxyWebsockets = true;
+        };
+
+        # Proxy Glances Web UI
+        locations."/diag/" = {
+          proxyPass = "http://localhost:61208/";
+        };      };
+    };
+  };
+
   environment.systemPackages = with pkgs; [
       jellyfin
       jellyfin-web
