7fa4f284ee295a053c4e8c1c5836edb104cf965c
Sean Carey <wycleffsean@gmail.com>
2025-12-23 16:29:29 -0500

    desktop: udisks2 for zfs I believe

--- targets/desktop_amd64/configuration.nix
+++ targets/desktop_amd64/configuration.nix
@@ -128,6 +128,11 @@
       '';
   };
 
+  services.udisks2 = {
+    enable = true;
+    # zfs.enable = true;
+  };
+
   services.hardware.openrgb = {
       enable = true;
       # package = pkgs.openrgb-with-all-plugins;
